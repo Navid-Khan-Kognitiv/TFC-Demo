ca44e902be0b9103e5bbc16bb74644a11cb533f9
Navid Khan <navidkhan@Navids-MacBook-Pro.local>
2023-01-14 15:20:36 -0500

    Created app Service B1 tier and added tags

--- app-service.tf
+++ app-service.tf
@@ -3,7 +3,7 @@ resource "azurerm_app_service_plan" "this" {
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   kind                = "Windows"
-
+  tags                = local.common_tags
   sku {
     tier = "Basic"
     size = "B1"
@@ -15,6 +15,7 @@ resource "azurerm_app_service" "example" {
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
   app_service_plan_id = azurerm_app_service_plan.this.id
+  tags                = local.common_tags
 
   site_config {
     dotnet_framework_version = "v4.0"
