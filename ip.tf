7f225d3354d3bcbd104dc0c78a1f237c83e77fe9
Navid Khan <navidkhan@Navids-MacBook-Pro.local>
2023-01-14 16:19:05 -0500

    Updated spelling errors

--- ip.tf
+++ ip.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "this" {
   name                = "dfgTestPublicIp1"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
-  allocation_method   = "Dyanmic"
+  allocation_method   = "Dynamic"
 
   tags = local.common_tags
 }
