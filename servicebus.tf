5197e8671ddd9881b975beaf0fd83f0ce864bd32
Navid Khan <navidkhan@Navids-MacBook-Pro.local>
2023-01-14 16:16:01 -0500

    Changed Tier froms tandard to Basic

--- servicebus.tf
+++ servicebus.tf
@@ -2,7 +2,7 @@ resource "azurerm_servicebus_namespace" "this" {
   name                = "dfg-servicebus-namespace"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
-  sku                 = "Standard"
+  sku                 = "Basic"
 
   tags = local.common_tags
 }
