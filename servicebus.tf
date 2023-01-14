6f79b728dcf5f225fe45c3ab700e4eb7fd18fd6c
Navid Khan <navidkhan@Navids-MacBook-Pro.local>
2023-01-14 16:14:20 -0500

    Added Service Bus

---
+++ servicebus.tf
@@ -0,0 +1,8 @@
+resource "azurerm_servicebus_namespace" "example" {
+  name                = "dfg-servicebus-namespace"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  sku                 = "Standard"
+
+  tags = local.common_tags
+}
